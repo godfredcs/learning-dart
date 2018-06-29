ee3c5bcf53f2f42bf8558ddf3455900b17a57095
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2018-06-29 23:35:27 +0000

    delved into variables where I pass through the var, num, int, double, String, bool ... and also const and final

---
+++ variables.dart
@@ -0,0 +1,39 @@
+// Variables
+
+main(List<String> args) {
+  var name = "Godfred"; // var is used for dynamic variables.
+  print(name);
+
+  name = "Larry";
+  print(name);
+
+  // Then there are the static variables.
+  num some_number = 2; // num is for "int" and "double".
+  print(some_number);
+
+  int number = 4;
+  print(number);
+
+  double another_number = 4.2;
+  print(another_number);
+
+  String gender = "male";
+  print(gender);
+
+  bool isTrue = true;
+  print(isTrue);
+
+  // var
+  // num
+  // int
+  // double
+  // String
+  // bool
+
+  // Then we have "const" and "final" Ok so final and const behave almost the same in they can't be reassigned.
+  const pie = 3.142;
+  final dream = 'viscious';
+  print(dream);
+  print(pie);
+
+}
